c3101d3f472ca2dbf555b209425a191945692370
Md Shayon <mdshayon0@gmail.com>
2021-10-10 05:32:06 +0600

    create simple list of directory, creating series tutorial page

--- coursetutorials/tutorials/views.py
+++ coursetutorials/tutorials/views.py
@@ -17,7 +17,32 @@ def contact(request):
 
 
 def tutorial_series(request):
-    return render(request, "tutorials/series-tutorials.html")
+    tutorials = ({
+            "num": 1,
+            "title": "Title one",
+            "desc": "This is desc one [Text editor text]",
+            "video_url": "https://www.youtube.com/watch?v=LzVzen_L2c8"
+        },
+        {
+            "num": 3,
+            "title": "Title three",
+            "desc": "This is desc three [Text editor text]",
+            "video_url": "https://www.youtube.com/watch?v=-6qNFvLUS9s"
+        },
+        {
+            "num": 2,
+            "title": "Title two",
+            "desc": "This is desc two [Text editor text]",
+            "video_url": "https://www.youtube.com/watch?v=QjoWsrzP-SI"
+        },
+    )
+
+    serilize_tutorials = sorted(tutorials, key=lambda x: x['num'])
+
+    context = {
+        "tutorials": serilize_tutorials
+    }
+    return render(request, "tutorials/series-tutorials.html", context)
 
 
 def individual_tutorial(request):
