7d145c5b1afab1c9449fe4653bb4696de2234f1c
Md Shayon <mdshayon0@gmail.com>
2021-10-10 14:04:26 +0600

    create tutorial models, admin setup, series tutorial templates

--- coursetutorials/static/js/main.js
+++ coursetutorials/static/js/main.js
@@ -7,7 +7,7 @@ const navLink = document.querySelectorAll('.nav-link');
 navLink.forEach((nl , i)=>{
     nl.addEventListener('click',e=>{
 //        e.preventDefault();
-        console.log(e.target);
+//        console.log(e.target);
 //         navLink.forEach(rmNL=>{
 //            if(rmNL.classList.contains("active")){
 //                 rmNL.classList.remove('active');
@@ -17,5 +17,28 @@ navLink.forEach((nl , i)=>{
         selectedNavLink.classList.add("active");
         selectedNavLink.setAttribute("aria-current", "page");
 
+    });
+});
+
+
+
+
+
+
+//SHOWING TAB CONTENT
+const tutorialTitle = document.querySelectorAll(".title-nav-link");
+const tutorialDesc = document.querySelectorAll('.tutorial-desc');
+const tabContent = document.getElementById("v-pills-tabContent");
+
+
+
+tutorialTitle.forEach((tt, i)=>{
+    tt.addEventListener('click', (e)=>{
+        tutorialDesc.forEach(td => td.classList.remove('active'));
+        const selectedTutorialNum =e.target.getAttribute('num');
+//        console.log(e.target.getAttribute('num'));
+        const selectedTutorial = tabContent.querySelector(`[num='${selectedTutorialNum}']`);
+//        console.log(selectedTutorial);
+        selectedTutorial.classList.add('active');
     });
 });
--- coursetutorials/tutorials/models.py
+++ coursetutorials/tutorials/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Tutorial(models.Model):
+    num = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=120, blank=False)
+    desc = models.TextField()
+    video_url = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.title
