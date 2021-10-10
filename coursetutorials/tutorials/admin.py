55562060621a6805a146c0c02f37af4f9cb201d0
Md Shayon <mdshayon0@gmail.com>
2021-10-10 14:36:59 +0600

    one to many relationship. getting some kind of error

--- coursetutorials/tutorials/admin.py
+++ coursetutorials/tutorials/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Tutorial
+from .models import Tutorial, TutorialSeries
 
 # Register your models here.
+admin.site.register(TutorialSeries)
 admin.site.register(Tutorial)
--- coursetutorials/tutorials/models.py
+++ coursetutorials/tutorials/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 
+
+    
+
+class TutorialSeries(models.Model):
+    series_title = models.CharField(max_length=120, blank=False)
+    series_desc = models.TextField()
+    # series_thumbnail =
+    # series_tags =
+    
+    def __str__(self):
+        return self.series_title
+
+
 # Create your models here.
 class Tutorial(models.Model):
     num = models.AutoField(primary_key=True)
     title = models.CharField(max_length=120, blank=False)
     desc = models.TextField()
     video_url = models.CharField(max_length=150)
+    # tags =
+    individual = models.BooleanField(default=True)  # SET TRUE IF IT IS SERIES
+    series = models.ForeignKey(TutorialSeries, on_delete=models.CASCADE, default="")
     
     def __str__(self):
         return self.title
