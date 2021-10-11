8063fc8f2fe30b58b74e0ba4593653c05fe7327f
Md Shayon <mdshayon0@gmail.com>
2021-10-11 23:03:06 +0600

    creating dependant field for model, solving some common problems

--- coursetutorials/tutorials/models.py
+++ coursetutorials/tutorials/models.py
@@ -13,6 +13,14 @@ class TutorialSeries(models.Model):
         return self.series_title
 
 
+
+# class Product(models.Model):
+#     fields here
+#
+#     def save(self, *args, **kwargs):
+#         self.tags = self.category + '' + self.subcategory
+#         super(Product, self).save(*args, **kwargs)
+
 # Create your models here.
 class Tutorial(models.Model):
     num = models.AutoField(primary_key=True)
@@ -21,7 +29,19 @@ class Tutorial(models.Model):
     video_url = models.CharField(max_length=150)
     # tags =
     individual = models.BooleanField(default=True)  # SET TRUE IF IT IS SERIES
-    series = models.ForeignKey(TutorialSeries, on_delete=models.CASCADE, default="")
+    series = models.ForeignKey(TutorialSeries, on_delete=models.CASCADE, default=None, blank=True, null=True)
     
+    
+    def save(self, *args, **kwargs):
+        # print(self.series)
+        # pass
+        # self.series
+        if self.series == None:
+            self.individual = True
+        else:
+            self.individual = False
+
+        super(Tutorial, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.title
